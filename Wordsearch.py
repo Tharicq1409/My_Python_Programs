0a8d830f5d2504e32b9752fd90cf3c45102fad52
Tharicq1409 <sheiktharicq14@gmail.com>
2023-04-26 05:49:01 +0530

    2programs added

---
+++ Wordsearch.py
@@ -0,0 +1,24 @@
+import os
+
+def findtext(dir, phrase):    
+    for file in os.listdir(dir):
+        path = os.path.join(dir.replace('/', '\\'), file) #path grabbing and converting the path into windows readable format
+        file_name = os.path.basename(file) # Getting Filename
+        if os.path.isdir(path):
+            findtext(path, phrase)
+        else:
+            try:
+                with open(path, 'r') as f:
+                    file_contents = f.read().lower()
+                    if phrase.lower() in file_contents:                                                                                                                                      
+                        #print(path)
+                        print(f"'{phrase}' found in '{file_name}' ||-->\t File Path :'{path}'\n")                       
+            except Exception as e:
+                print(f"Error reading file '{path}': {e}")
+    
+# Directory and search phrase user Input 
+dir_path = input("Enter Directory:")
+search_phrase = input("Enter search phrase: ")
+
+#Function call
+findtext(dir_path, search_phrase)
---
+++ csvfilecompare.py
@@ -0,0 +1,36 @@
+import csv
+
+# open the two CSV files
+with open('C:/Users/3iintr00203/Desktop/integrations list/completed.csv') as file1, open('C:/Users/3iintr00203/Desktop/integrations list/all.csv') as file2:
+    # read the contents of each file into a list
+    file1_list = [row for row in csv.reader(file1)]
+    file2_list = [row for row in csv.reader(file2)]
+    
+    # find the unique strings in each file
+    file1_set = set([item for sublist in file1_list for item in sublist])
+    file2_set = set([item for sublist in file2_list for item in sublist])
+    
+    # print the unique strings in each file
+    print("Unique strings in file 1:", file1_set - file2_set)
+    print("Unique strings in file 2:", file2_set - file1_set)
+
+'''
+import csv
+
+# open the two CSV files
+with open('C:/Users/3iintr00203/Desktop/integrations list/completed.csv') as file1, open('C:/Users/3iintr00203/Desktop/integrations list/all.csv') as file2, open('C:/Users/3iintr00203/Desktop/integrations list/output.csv', 'w', newline='') as output_file:
+    # read the contents of each file into a list
+    file1_list = [row for row in csv.reader(file1)]
+    file2_list = [row for row in csv.reader(file2)]
+    
+    # find the unique strings in each file
+    file1_set = set([item for sublist in file1_list for item in sublist])
+    file2_set = set([item for sublist in file2_list for item in sublist])
+    
+    # write the unique strings to the output file
+    writer = csv.writer(output_file)
+    writer.writerow(['Unique strings in file 1'])
+    writer.writerow(list(file1_set - file2_set))
+    writer.writerow(['Unique strings in file 2'])
+    writer.writerow(list(file2_set - file1_set))
+'''
